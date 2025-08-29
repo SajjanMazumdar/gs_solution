326886c4a4e2cd5e12cb70b80206cbb360003a94
sajjan <sajjan.mazumdar@gmail.com>
2025-08-29 17:54:11 +0530

    setup enviroment in localhost

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     gui_mode: false,
-    loginUrl: "http://223.182.122.147:8081/api/login/",
-    masterUrl: "http://223.182.122.147:8081/api/master/",
+    loginUrl: "http://localhost:3001/",
+    masterUrl: "http://localhost:3002/",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     gui_mode: true,
-    loginUrl: "http://223.182.122.147:8081/api/login/",
-    masterUrl: "http://223.182.122.147:8081/api/master/",
+    loginUrl: "http://localhost:3001/",
+    masterUrl: "http://localhost:3002/",
 };
